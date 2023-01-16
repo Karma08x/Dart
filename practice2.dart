0bf22e7e7660f1263389b95fdd84e320318a65fa
Samriddha <tamrakarkarma@gmail.com>
2023-01-16 15:12:41 +0545

    syncprocess

--- practice2.dart
+++ practice2.dart
@@ -32,11 +32,59 @@ void main() {
 //   print('Karma');
 // }
 
-  stdout.write("Enter number: ");
-  int num = int.parse(stdin.readLineSync()!);
-  if (num > 0) {
-    print(num + num);
-  } else {
-    print('This is a negative number.');
-  }
+//(5)___stdout.write("Enter number: ");
+// int num = int.parse(stdin.readLineSync()!);
+// var sum = 0;
+// for (var i = 1; i <= num; i++) {
+//   sum += i;
+// }
+// print('The sum of first $num natural numbers is $sum.');
+
+//(6)__for (int i = 1; i <= 10; i++) {
+//   print('5 * $i = ${5 * i}');
+// }
+
+//(7)___for (int i = 1; i <= 10; i++) {
+//   for (int j = 1; j <= 10; j++) {
+//     print('$i * $j = ${i * j}');
+//   }
+// }
+
+//(8)___stdout.write('Enter your first number: ');
+// var num1 = double.parse(stdin.readLineSync()!);
+
+// stdout.write('Enter your second number: ');
+// var num2 = double.parse(stdin.readLineSync()!);
+
+// stdout.write('Choose an operator(+,-,*,/): ');
+// var oper = stdin.readLineSync();
+
+// double Result;
+
+// switch (oper) {
+//   case "+":
+//     Result = num1 + num2;
+//     print('this is the result: $Result');
+//     break;
+//   case "-":
+//     Result = num1 - num2;
+//     print('this is the result: $Result');
+//     break;
+//   case "*":
+//     Result = num1 * num2;
+//     print('this is the result: $Result');
+//     break;
+//   case "/":
+//     Result = num1 / num2;
+//     print('this is the result: $Result');
+//     break;
+//   default:
+//     print("Invalid operation");
+// }
+
+//(9)___for (int i = 1; i <= 100; i++) {
+//   if (i != 41) {
+//     print(i);
+//   }
+// }
 }
