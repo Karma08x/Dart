719cb125256010d63f4298e00663b6e1fe4510fb
Samriddha <tamrakarkarma@gmail.com>
2023-01-16 11:50:01 +0545

    Function

---
+++ 13Function.dart
@@ -0,0 +1,10 @@
+void main() {
+  tarkariLiyeraAauni();
+}
+
+void tarkariLiyeraAauni() {
+  print('First get up:');
+  print('Make a list of vegetables: ');
+  print('BUy them:');
+  print('Bring it all back');
+}
