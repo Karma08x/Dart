d4c5dc33aa1da20473ffc8d3fe93fc1510573553
Samriddha <tamrakarkarma@gmail.com>
2023-01-16 12:25:54 +0545

    Syncprocess

--- practice2.dart
+++ practice2.dart
@@ -1,12 +1,30 @@
 import 'dart:io';
 
 void main() {
-  //(1)___ stdout.write('Enter your number: ');
-  // int number = int.parse(stdin.readLineSync()!);
+//(1)___ stdout.write('Enter your number: ');
+// int number = int.parse(stdin.readLineSync()!);
 
-  // if (number % 2 == 0) {
-  //   print('Even');
-  // } else {
-  //   print('ODD');
-  // }
+// if (number % 2 == 0) {
+//   print('Even');
+// } else {
+//   print('ODD');
+// }
+
+//(2)__stdout.write('Enter your character: ');
+// String? charac = stdin.readLineSync();
+// if (['a', 'e', 'i', 'o', 'u'].contains(charac?.toLowerCase())) {
+//   print('The character $charac is vowel.');
+// } else {
+//   print('The character $charac is consonant.');
+// }
+
+//(3)___stdout.write('Enter your number: ');
+// int num = int.parse(stdin.readLineSync()!);
+// if (num > 0) {
+//   print('The number $num is positive');
+// } else if (num < 0) {
+//   print('The number $num is negative');
+// } else {
+//   print('The number is zero.');
+// }
 }
