245d2b20fd3a60c8d973cbdf52a448c56a33daf4
Samriddha <tamrakarkarma@gmail.com>
2023-01-19 14:15:44 +0545

    New commit

--- Ans_for_practice/practice3.dart
+++ Ans_for_practice/practice3.dart
@@ -1,7 +1,21 @@
-void name() {
-  print('Samriddha Tamrakar');
-}
+//(1)____void name() {
+//   print('Samriddha Tamrakar');
+// }
 
-void main() {
-  name();
-}
+// void main() {
+//   name();
+// }
+
+//(2)___void printEvenNumbers(int start, int end) {
+//   for (int i = start; i <= end; i++) {
+//     if (i % 2 == 0) {
+//       print(i);
+//     }
+//   }
+// }
+
+// void main() {
+//   int start = 2;
+//   int end = 10;
+//   printEvenNumbers(start, end);
+// }
