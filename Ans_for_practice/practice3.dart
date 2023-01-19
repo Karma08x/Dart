d98ed8f0a121ae93f9e333200a53b0043632c0c5
Samriddha <tamrakarkarma@gmail.com>
2023-01-19 14:20:31 +0545

    New commit

--- Ans_for_practice/practice3.dart
+++ Ans_for_practice/practice3.dart
@@ -19,3 +19,27 @@
 //   int end = 10;
 //   printEvenNumbers(start, end);
 // }
+
+//(3)___import 'dart:math';
+
+// String generateRandomPassword(int length) {
+//   String characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+//   String password = '';
+//   Random random = new Random();
+//   for (int i = 0; i < length; i++) {
+//     password += characters[random.nextInt(characters.length)];
+//   }
+//   return password;
+// }
+
+//(4)___import 'dart:math';
+
+// double findAreaOfCircle(double radius) {
+//   return pi * pow(radius, 2);
+// }
+
+//(5)___import 'dart:math';
+
+// double pythagoreanTheorem(double a, double b) {
+//   return sqrt(pow(a, 2) + pow(b, 2));
+// }
