e63825b461f47025e96ccdf0d956bccb74e24446
Samriddha <tamrakarkarma@gmail.com>
2023-01-24 16:41:22 +0545

    commit

--- Ans_for_practice/practice3.dart
+++ Ans_for_practice/practice3.dart
@@ -6,13 +6,13 @@
 //   name();
 // }
 
-//(2)___void printEvenNumbers(int start, int end) {
-//   for (int i = start; i <= end; i++) {
-//     if (i % 2 == 0) {
-//       print(i);
-//     }
-//   }
-// }
+void printEvenNumbers(int start, int end) {
+  for (int i = start; i <= end; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+}
 
 // void main() {
 //   int start = 2;
