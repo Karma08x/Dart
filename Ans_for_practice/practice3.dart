f8a329008f4582fea4d5e03370038dc045510761
Samriddha <tamrakarkarma@gmail.com>
2023-01-24 17:00:23 +0545

    initial commit

--- Ans_for_practice/practice3.dart
+++ Ans_for_practice/practice3.dart
@@ -6,19 +6,19 @@
 //   name();
 // }
 
-void printEvenNumbers(int start, int end) {
-  for (int i = start; i <= end; i++) {
-    if (i % 2 == 0) {
-      print(i);
-    }
-  }
-}
+//(2)___void printEvenNumbers(int start, int end) {
+//   for (int i = start; i <= end; i++) {
+//     if (i % 2 == 0) {
+//       print(i);
+//     }
+//   }
+// }
 
-void main() {
-  int start = 2;
-  int end = 10;
-  printEvenNumbers(start, end);
-}
+// void main() {
+//   int start = 2;
+//   int end = 10;
+//   printEvenNumbers(start, end);
+// }
 
 //(3)___import 'dart:math';
 
@@ -32,14 +32,45 @@ void main() {
 //   return password;
 // }
 
-//(4)___import 'dart:math';
+// void main() {
+//   print(generateRandomPassword(8));
+// }
+
+//(4)____import 'dart:math';
 
 // double findAreaOfCircle(double radius) {
 //   return pi * pow(radius, 2);
+// }
+
+// void main() {
+//   print(findAreaOfCircle(1));
 // }
 
 //(5)___import 'dart:math';
 
 // double pythagoreanTheorem(double a, double b) {
 //   return sqrt(pow(a, 2) + pow(b, 2));
+// }
+
+// void main() {
+//   print(pythagoreanTheorem(4, 5));
+// }
+
+//(6)___String reverseString(String input) {
+//   return input.split('').reversed.join();
+// }
+
+// void main() {
+//   print(reverseString('Hello, World!'));
+// }
+
+
+
+//(7)___import 'dart:math';
+
+// void main() {
+//   num base = 5;
+//   num exponent = 3;
+//   num result = pow(base, exponent);
+//   print("$base ^ $exponent = $result");
 // }
